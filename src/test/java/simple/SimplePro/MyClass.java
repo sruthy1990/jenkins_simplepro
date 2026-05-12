cbc9d7f60c86c5e2ae4ca3e009d4a4baa7ece6ef
Sruthy Sunil <sruthyvsunil@gmail.com>
2026-05-12 14:44:24 +0530

    initial commit

---
+++ src/test/java/simple/SimplePro/MyClass.java
@@ -0,0 +1,14 @@
+package simple.SimplePro;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class MyClass {
+	
+	@Test
+	public void hello()
+	{
+		System.out.println("Hello World");
+	}
+
+}
