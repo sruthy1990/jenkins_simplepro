942283dbdc61912d8e7c8d7facd6a0971fbfe479
Sruthy Sunil <sruthyvsunil@gmail.com>
2026-05-13 16:49:08 +0530

    commitchange

--- src/test/java/simple/SimplePro/MyClass.java
+++ src/test/java/simple/SimplePro/MyClass.java
@@ -8,7 +8,7 @@ public class MyClass {
 	@Test
 	public void hello()
 	{
-		System.out.println("Hai,myself Sruthy");
+		System.out.println("Hai,myself ......Sruthy");
 		System.out.println("jenkins must be invoked automatically");
 	}
 
